ba9fde6a1018fa558b71ade4b8025e28debc53b1
daniel-llach <daniel.llach@gmail.com>
2015-04-04 02:11:56 -0300

    fix uploadfiles

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,11 @@ require.config({
         underscore: {
             exports: "_"
         },
+        marionette:{
+            deps: [
+                "backbone"
+            ]
+        },
         backbone: {
             deps: [
                 "underscore",
@@ -20,6 +25,7 @@ require.config({
     paths: {
         jquery: "../bower_components/jquery/jquery",
         underscore: "../bower_components/underscore/underscore",
+        backbone: "../bower_components/backbone/backbone",
         marionette: '../bower_components/marionette/lib/backbone.marionette',
         text: "../bower_components/requirejs-text/text"
     }
