1e145aa91e21f9ad92a9781b82962dd3c09389ad
daniel-llach <daniel.llach@gmail.com>
2015-04-03 22:20:28 -0300

    add header

--- public/js/appview.js
+++ public/js/appview.js
@@ -1,17 +1,41 @@
 /*global define*/
 define([
     "backbone",
-    "marionette"
-], function (Backbone, Marionette) {
+    "marionette",
+    "text!templates/header/header.html"
+], function (Backbone, Marionette, HeaderTemplate) {
     "use strict";
 
-    var AppView = Backbone.View.extend({
-
-        initialize: function () {
-           // alert('ehhhh!!!');
-        }
-
+    AppView = new Backbone.Marionette.Application();
+    AppView.addRegions({
+        'header': 'header',
+        'maincontent': '.content',
+        'footer': 'footer'
     });
+
+    var HeaderLayout = Backbone.Marionette.Layout.extend({
+        template: _.template(HeaderTemplate)
+    });
+
+    AppView.addInitializer(function(options) {
+        var headerLayout = new headerLayout();
+
+        AppView.header.show(headerLayout);
+    });
+
+    // var AppView = Backbone.View.extend({
+
+    //     regions: {
+    //         header: "header",
+    //         maincontent: ".content",
+    //         footer: "footer"
+    //     },
+
+    //     initialize: function () {
+    //        // alert('ehhhh!!!');
+    //     }
+
+    // });
 
     return AppView;
 });
