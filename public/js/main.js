6923b0caa941b4c34b5599c07fd4fc34f2cdfa9a
daniel-llach <daniel.llach@gmail.com>
2015-04-06 16:31:26 -0300

    fix dependencies

--- public/js/main.js
+++ public/js/main.js
@@ -18,11 +18,11 @@ require.config({
         }
     },
     paths: {
-        jquery: "../bower_components/jquery/jquery",
+        jquery: "../bower_components/jquery/dist/jquery",
         underscore: "../bower_components/underscore/underscore",
         backbone: "../bower_components/backbone/backbone",
-        marionette: '../bower_components/marionette/lib/backbone.marionette',
-        text: "../bower_components/requirejs-text/text"
+        marionette: '../bower_components/backbone.marionette/backbone.marionette',
+        text: "../bower_components/requirejs/require"
     }
 });
 
